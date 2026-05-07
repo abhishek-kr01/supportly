b4efc81f90ce51ff1c04b80c7465352e77e7f33d
Abhishek Kumar <abhishekmdp11@gmail.com>
2026-05-08 00:32:28 +0530

    integrate Scalekit auth with session management

---
+++ src/lib/getSession.ts
@@ -0,0 +1,23 @@
+import { cookies } from "next/headers";
+import { scalekit } from "./scalekit";
+
+export async function getSession() {
+    const session=await cookies()
+    const token = session.get("access_token")?.value;
+
+    if(!token){
+        return null;
+    }
+
+    try {
+        const result:any = await scalekit.validateToken(token);
+        const user = await scalekit.user.getUser(result.sub)
+        return user;
+    } catch (error) {
+        console.log(error)
+    }
+
+    console.log(token)
+    
+    // const user = 
+}
